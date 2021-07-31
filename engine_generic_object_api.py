29a655773fcbc6c04f9cda4a2ddd450637a89254
Bintocher <schernov1@gmail.com>
2021-07-31 22:33:52 +0300

    updated from https://github.com/gchehab/pyqlikengine

--- engine_generic_object_api.py
+++ engine_generic_object_api.py
@@ -14,8 +14,25 @@ class EngineGenericObjectApi:
         except KeyError:
             return response["error"]
 
+    def get_properties(self, handle):
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetProperties", "params": []})
+        response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
+        try:
+            return response["result"]
+        except KeyError:
+            return response["error"]
+
     def get_hypercube_data(self, handle, path="/qHyperCubeDef", pages=[]):
         msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetHyperCubeData",
+                          "params": [path,pages]})
+        response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
+        try:
+            return response["result"]
+        except KeyError:
+            return response["error"]
+
+    def get_hypercube_pivot_data(self, handle, path="/qHyperCubeDef", pages=[]):
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetHyperCubePivotData",
                           "params": [path,pages]})
         response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
         try:
@@ -26,6 +43,15 @@ class EngineGenericObjectApi:
     def get_list_object_data(self, handle, path="/qListObjectDef", pages=[]):
         msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "GetListObjectData",
                           "params": [path, pages]})
+        response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
+        try:
+            return response["result"]
+        except KeyError:
+            return response["error"]
+
+    def expand_left(self, handle, path="/qHyperCubeDef", row=0, col=0, all=True):
+        msg = json.dumps({"jsonrpc": "2.0", "id": 0, "handle": handle, "method": "ExpandLeft",
+                          "params": [path, row, col, all]})
         response = json.loads(self.engine_socket.send_call(self.engine_socket, msg))
         try:
             return response["result"]
